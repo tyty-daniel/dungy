542dfc43e1b0340a0b3938f773ef0b7129eabf81
tyty-daniel <magmahat@gmail.com>
2020-11-15 19:56:59 -0100

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         resolve: 'gatsby-source-filesystem',
         options: {
           name: 'blog',
-          path: `${__dirname}/src/pages/blog`,
+          path: `${__dirname}/blog`,
         },
       },
       'gatsby-transformer-remark',
