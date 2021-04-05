c248db06e262268a59e7f960882cb0e17e3795c9
tyty-daniel <magmahat@gmail.com>
2021-04-05 21:04:36 +0100

    about.js fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Video from "../components/video"
+import SEO from "../components/seo"
 
 export default function About() {
     return (
